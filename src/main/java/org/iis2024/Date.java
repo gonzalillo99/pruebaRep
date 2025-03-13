6754205b94a9a497a299dde4a54ed7535e8b6c9e
Nestordz <nestorvaldemoro@gmail.com>
2025-03-13 16:33:31 +0100

    Modificacion colabolador

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -19,6 +19,7 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
+  private final minutes;
 
   /**
    * Retrieves the day component of this date.
